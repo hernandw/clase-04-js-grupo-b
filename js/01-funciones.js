3bb9113ab10973d34554823a1ed84ec3615b3478
Williams Hernandez <hernandw@gmail.com>
2026-01-28 19:04:45 -0300

    :tada: creacion de la estructura del proyecto y definicion de funciones

---
+++ js/01-funciones.js
@@ -0,0 +1,22 @@
+//Funciones: son un bloque de código reutilizable que se puede llamar en cualquier momento
+
+
+
+//definir la función
+function saludo(){
+    //bloque de instrucciones
+
+    console.log("Hola buenas tardes")
+}
+
+
+function sumar(){
+    console.log(4+5)
+}
+
+//llamarla o invocarla
+
+saludo()
+saludo()
+sumar()
+sumar()
